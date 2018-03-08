499672ce8242f2cedb1072c5ecc07d40bcf6b024
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-03-08 16:30:34 +0530

    Create p211.c

---
+++ p211.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+int a,b,d;
+
+printf("enter the base and height");
+scanf("%d%d",&a,&b);
+d=(a*b)/2;
+printf("area is %d",d);
+return 0;
+}
+
+
+
