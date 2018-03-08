c1312cf8b9c24fe4176cf1070dbfb8cba36f7986
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-03-08 15:54:22 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+char ch[100];
+printf("enter the day");
+scanf("%c",&ch);
+if(ch=='s'||ch=='S')
+printf("holiday");
+else
+printf("working day");
+    return 0;
+}
+
+
+
